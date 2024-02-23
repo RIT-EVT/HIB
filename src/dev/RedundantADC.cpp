8baa2fbd4506fc38f1b6c912c806aad95d9d93e5
Oleg <lazarioleg97@gmail.com>
2024-02-22 20:16:42 -0500

    some changes

--- src/dev/RedundantADC.cpp
+++ src/dev/RedundantADC.cpp
@@ -5,8 +5,8 @@ namespace IO = EVT::core::IO;
 
 namespace RedundantADC {
 
-/**
- * @brief Constructs a RedundantADC object with the given ADC instances.
+/*
+ * Constructs a RedundantADC object with the given ADC instances.
  *
  * @param[in] adc0 The first ADC instance.
  * @param[in] adc1 The second ADC instance.
@@ -14,15 +14,15 @@ namespace RedundantADC {
  */
 RedundantADC::RedundantADC(IO::ADC& adc0, IO::ADC& adc1, IO::ADC& adc2) : adc0(adc0), adc1(adc1), adc2(adc2) {}
 
-/**
- * @brief Process readings from the ADCs and check for redundancy.
+/*
+ * Process readings from the ADCs and check for redundancy.
  *
  * This function reads values from three ADCs, performs bitwise comparison,
  * and checks for redundancy among the readings.
  *
- * @param[in] val1 Reference to the variable to store the value read from the first ADC.
- * @param[in] val2 Reference to the variable to store the value read from the second ADC.
- * @param[in] val3 Reference to the variable to store the value read from the third ADC.
+ * @param[out] val1 Reference to the variable to store the value read from the first ADC.
+ * @param[out] val2 Reference to the variable to store the value read from the second ADC.
+ * @param[out] val3 Reference to the variable to store the value read from the third ADC.
  * @return RedundantADC::Status The status of the processing.
  */
 RedundantADC::Status RedundantADC::readVoltage(uint32_t& val1, uint32_t& val2, uint32_t& val3) {
@@ -32,9 +32,9 @@ RedundantADC::Status RedundantADC::readVoltage(uint32_t& val1, uint32_t& val2, u
     auto adc2ValueUint = static_cast<uint32_t>(adc2.read() * 1000);
 
     // Calculate differences
-    uint32_t diff01 = adc0ValueUint - adc1ValueUint;
-    uint32_t diff02 = adc0ValueUint - adc2ValueUint;
-    uint32_t diff12 = adc1ValueUint - adc2ValueUint;
+    int32_t diff01 = adc0ValueUint - adc1ValueUint;
+    int32_t diff02 = adc0ValueUint - adc2ValueUint;
+    int32_t diff12 = adc1ValueUint - adc2ValueUint;
 
     // Check for exact match
     if (diff01 == 0 || diff02 == 0 || diff12 == 0) {
