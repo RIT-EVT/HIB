84ff3b91b45ae2e78f9933b216daa7145b085274
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2024-09-29 20:51:26 -0400

    Added new git branch

--- include/HIB.hpp
+++ include/HIB.hpp
@@ -1,13 +1,25 @@
 #pragma once
 
+#include <cstdint>
+
 namespace HIB {
 
 /**
  * HIB header file
  */
 class HIB {
-public:
 private:
+  uint16_t throttlePosition;
+  uint16_t brakePosition;
+  uint8_t offByOneErrors;
+  uint8_t marginErrors;
+  uint8_t comparisonErrors;
+  uint8_t thresholdInformation;
+public:
+  /// Creates a payload for the CAN bus to transmit.
+  uint8_t* makePayload();
+  uint16_t getThrottlePosition();
+  uint16_t getBrakePosition();
 };
 
 }// namespace HIB
